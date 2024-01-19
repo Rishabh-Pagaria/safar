6243de385a25bdc184a14eed5f6324ac1bee0b6b
Rishabh-Pagaria <rpagaria2000@gmail.com>
2024-01-19 12:07:54 +0530

    pushing the site changes

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ app.set("view engine", "ejs")
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
 app.get("/",(req,res)=>{
     res.render("homepage.ejs");
+})
+app.get("/about",(req,res)=>{
+    res.render("about.ejs");
+})
+app.get("/destinations",(req,res)=>{
+    res.render("destinations.ejs");
+})
+app.get("/contacts",(req,res)=>{
+    res.render("contact.ejs");
 })
 
 app.listen(port,()=>{
